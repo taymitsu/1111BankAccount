a0ae37a3713a0a130cc32506fdcb75288b32b5e9
taymitsu <tmitsumori96@gmail.com>
2023-10-25 14:19:12 -0700

    MitchelAccount added

--- BankAccount.py
+++ BankAccount.py
@@ -24,16 +24,34 @@ class BankAccount():
     print(f'Account Balance: ${self.balance}')
     return self.balance
 
-  def print_details(self):
+  def add_interest(self):
+    self.balance * 0.00083
+    print(f'Interest applied. New Account Balance: ${self.balance}')
+
+  def print_statement(self):
     print(self.full_name)
     print(f'Account No.: {self.account_number}')
     self.get_balance()
 
-tayAccount = BankAccount("Taylor", "444")
-#tayAccount.print_details()
-tayAccount.get_balance() 
-tayAccount.deposit(11)
-#tayAccount.print_details()
+
+
+MitchellAccount = BankAccount("Mitchell", "03141592")
+MitchellAccount.deposit(400000)
+MitchellAccount.print_statement()
+MitchellAccount.add_interest()
+MitchellAccount.print_statement()
+MitchellAccount.withdraw(150)
+MitchellAccount.print_statement()
+
+
+
+#tayAccount = BankAccount("Taylor", "444")
+#tayAccount.print_statement()
+#tayAccount.get_balance() 
+#tayAccount.deposit(11)
+#tayAccount print_statement()
 #tayAccount.withdrawal(9)
-tayAccount.withdraw(90) #testing else statement#
-#tayAccount.print_details()
+#tayAccount.withdraw(90) #testing else statement#
+#tayAccount print_statement()
+#tayAccount.print_statement()
+#tayAccount.add_interest()
