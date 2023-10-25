2c78b7ca2517f353eed7ae2f81836a96db64a55a
taymitsu <tmitsumori96@gmail.com>
2023-10-25 14:34:27 -0700

    interest method debugged

--- BankAccount.py
+++ BankAccount.py
@@ -25,7 +25,8 @@ class BankAccount():
     return self.balance
 
   def add_interest(self):
-    self.balance * 0.00083
+    interest = self.balance * 0.00083
+    self.balance += interest
     print(f'Interest applied. New Account Balance: ${self.balance}')
 
   def print_statement(self):
@@ -33,25 +34,24 @@ class BankAccount():
     print(f'Account No.: {self.account_number}')
     self.get_balance()
 
-
-
-MitchellAccount = BankAccount("Mitchell", "03141592")
-MitchellAccount.deposit(400000)
-MitchellAccount.print_statement()
-MitchellAccount.add_interest()
-MitchellAccount.print_statement()
-MitchellAccount.withdraw(150)
-MitchellAccount.print_statement()
-
-
-
-#tayAccount = BankAccount("Taylor", "444")
+tayAccount = BankAccount("Taylor", "444")
 #tayAccount.print_statement()
 #tayAccount.get_balance() 
-#tayAccount.deposit(11)
+tayAccount.deposit(11)
 #tayAccount print_statement()
-#tayAccount.withdrawal(9)
+tayAccount.withdraw(9)
 #tayAccount.withdraw(90) #testing else statement#
-#tayAccount print_statement()
-#tayAccount.print_statement()
-#tayAccount.add_interest()
+tayAccount.print_statement()
+
+tayAccount.add_interest()
+tayAccount.print_statement()
+
+#MitchellAccount = BankAccount("Mitchell", "03141592")
+#MitchellAccount.deposit(400000)
+#MitchellAccount.print_statement()
+#MitchellAccount.add_interest()
+#MitchellAccount.print_statement()
+#MitchellAccount.withdraw(150)
+#MitchellAccount.print_statement()
+
+
