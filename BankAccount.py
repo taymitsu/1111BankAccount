f60bbe6e3053159980af19f253aa984267437458
taymitsu <tmitsumori96@gmail.com>
2023-10-25 13:37:18 -0700

    init method corrected

--- BankAccount.py
+++ BankAccount.py
@@ -1,8 +1,9 @@
 
-class BankAccount(self, full_name, account_number, balance=0):
-  self.full_name = full_name
-  self.account_number = account_number
-  self.balance = balance
+class BankAccount():
+  def __init__(self, full_name, account_number, balance=0):
+    self.full_name = full_name
+    self.account_number = account_number
+    self.balance = balance
 
   def get_balance(self):
     print(f'Account Balance: {self.balance}')
@@ -12,3 +13,7 @@ class BankAccount(self, full_name, account_number, balance=0):
     print(self.full_name)
     print(f'Account No.: {self.account_number}')
     self.get_balance()
+
+#tayAccount = BankAccount("Taylor", "444")
+#tayAccount.print_details()
+#tayAccount.get_balance() still returnign zero, need to add deposit/withdrawl
