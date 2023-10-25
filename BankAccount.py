79446bce0a67258ad9caad7beb2f9ac4e538fcb0
taymitsu <tmitsumori96@gmail.com>
2023-10-25 14:06:28 -0700

    withdraw method added

--- BankAccount.py
+++ BankAccount.py
@@ -10,7 +10,15 @@ class BankAccount():
     print(f'Amount deposited: ${amount}')
     print(f'New Account Balance: ${self.balance}\n')
 
-  #def withdrawal(self, amount): IF STATEMENT
+  def withdraw(self, amount): #IF STATEMENT
+    if self.balance > amount:
+      self.balance -= amount
+      print(f'Amount Withdrawn: ${amount}')
+      print(f'New Account Balance: ${self.balance}\n')
+    else:
+      print('Cannot complete transaction: INSUFFICIENT FUNDS')
+      print('A $10 overdraft fee has been applied to your account.\n')
+      print(f'Current Account Balance: ${self.balance - 10}')
 
   def get_balance(self):
     print(f'Account Balance: ${self.balance}')
@@ -21,8 +29,11 @@ class BankAccount():
     print(f'Account No.: {self.account_number}')
     self.get_balance()
 
-#tayAccount = BankAccount("Taylor", "444")
+tayAccount = BankAccount("Taylor", "444")
 #tayAccount.print_details()
-#tayAccount.get_balance() still returnign zero, need to add deposit/withdrawl
-#tayAccount.deposit(11)
+tayAccount.get_balance() 
+tayAccount.deposit(11)
+#tayAccount.print_details()
+#tayAccount.withdrawal(9)
+tayAccount.withdraw(90) #testing else statement#
 #tayAccount.print_details()
